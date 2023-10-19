7a7a6e5437c1015823d02de92e528f7d278d43c0
AlexanderTL0343 <145624722+AlexanderTL0343@users.noreply.github.com>
2023-10-18 20:39:52 -0600

    Add files via upload

---
+++ colegiado/src/colegiado/spotify.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colegiado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alext
+ */
+public class spotify {
+    private int duracion;
+    private String titulo;
+    private String autor;
+    private int id_de_la_cancion;
+    private int fecha_de_lanzamiento;
+    
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getId_de_la_cancion() {
+        return id_de_la_cancion;
+    }
+
+    public void setId_de_la_cancion(int id_de_la_cancion) {
+        this.id_de_la_cancion = id_de_la_cancion;
+    }
+
+    public int getFecha_de_lanzamiento() {
+        return fecha_de_lanzamiento;
+    }
+
+    public void setFecha_de_lanzamiento(int fecha_de_lanzamiento) {
+        this.fecha_de_lanzamiento = fecha_de_lanzamiento;
+    }
+   
+}
