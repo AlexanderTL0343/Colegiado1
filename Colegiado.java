083231db27d58a0dfbec0e6225a2599abfb62f5a
AlexanderTL0343 <145624722+AlexanderTL0343@users.noreply.github.com>
2023-10-18 20:29:43 -0600

    Add files via upload

---
+++ Colegiado.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package colegiado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alext
+ */
+public class Colegiado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     spotify cancion= new spotify();   
+     cancion.getAutor();
+     cancion.getTitulo();
+     cancion.getId_de_la_cancion();
+     cancion.getFecha_de_lanzamiento();
+     cancion.getDuracion();
+     
+    }
+   
+}
